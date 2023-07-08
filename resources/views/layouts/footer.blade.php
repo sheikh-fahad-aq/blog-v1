efaa039e86ca958c51856fa77d4df9ba7d63351c
sheikh-fahad-aq <fahadahmedqureshi.93@gmail.com>
2023-07-08 21:14:40 +0500

    Home with Login & Register done

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,65 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pg-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Home') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Blog') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Login') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Register') }}</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find Us
+            </h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('What we do') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Address') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Phone') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Contact') }}</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Latest Post
+            </h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Why we love tech') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Why we love design') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Why to use Laravel') }}</a>
+                </li>
+                <li class="pb-1">
+                    <a class="" href="/">{{ __('Why PHP is the best') }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="pb-3 m-auto text-us text-gray-100 pt-6">
+        Copyright <?= date('Y') ?> Fahad-Ahmed-Qureshi Dev. All Rights Reserved
+    </p>
+</footer>
